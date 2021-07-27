fb33479645915b950a880ab8a53e385461a21ba0
komukai-wa <kvku20907@gmail.com>
2021-07-27 21:15:21 +0900

    制御文　再提出
    制御文　再提出

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -43,17 +43,16 @@ if(name.equals(USER_NAME)&&pass.equals(USER_PASSWORD)){
 System.out.println(CONST_MSG_SUCCESS);}
 // ③ 「USER_RNAME」の値のみ等しい場合。
  //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
-       if(pass.equals(USER_PASSWORD));
-       else {System.out.println(CONST_MSG_ERROR_PASS);}
+ if(pass.equals(USER_PASSWORD));
+ else if(name.equals(USER_NAME)){System.out.println(CONST_MSG_ERROR_PASS);}
 // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
        if(name.equals(USER_NAME));
-else {System.out.println(CONST_MSG_ERROR_NAME);
-        
-    }
-  // ⑤ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。 
+       else if (pass.equals(USER_PASSWORD)){System.out.println(CONST_MSG_ERROR_NAME);
+       }
+       // ⑤ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。 
        //定数を使用して「 入力情報に誤りがあります。 」と出力してください。
-       if(name.equals(USER_NAME)&&pass.equals(USER_PASSWORD)){}
-       else {System.out.println(CONST_MSG_ERROR_INPUT);}
-}
+       if(!name.equals(USER_NAME)&&!pass.equals(USER_PASSWORD)){System.out.println(CONST_MSG_ERROR_INPUT);}
+
+       }
 }
